9478e4ee67eb699128eb7b6671a31f3e5c16772e
ahmedhmmad <Ahmed.hammad@students.iugaza.edu.ps>
2024-04-20 03:00:30 +0200

    customerController

--- routes/deposit.js
+++ routes/deposit.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const db = require('../utils/db');
+const customerController=require('../controllers/customerController')
 
 const router = express.Router();
 
@@ -10,13 +11,13 @@ router.post('/', async (req, res) => {
     try {
         // Retrieve customer's current balance
         const connection = await db.getConnection();
-        const currentBalance = await getCustomerBalance(connection, customerId);
+        const currentBalance = await customerController.getCustomerBalance(connection, customerId);
 
         // Calculate new balance after deposit
         const newBalance = currentBalance + amount;
 
         // Update customer's balance in the database
-        await updateCustomerBalance(connection, customerId, newBalance);
+        await customerController.updateAccountBalance(connection, customerId, newBalance);
         connection.release();
 
         res.status(200).json({ message: "Deposit successful", newBalance });
@@ -26,29 +27,29 @@ router.post('/', async (req, res) => {
     }
 });
 
-// Function to retrieve customer's current balance from the database
-const getCustomerBalance = async (connection, customerId) => {
-    return new Promise((resolve, reject) => {
-        connection.query('SELECT balance FROM customers WHERE customer_id = ?', [customerId], (error, result) => {
-            if (error) {
-                reject(error);
-            } else {
-                resolve(result[0].balance);
-            }
-        });
-    });
-};
+// // Function to retrieve customer's current balance from the database
+// const getCustomerBalance = async (connection, customerId) => {
+//     return new Promise((resolve, reject) => {
+//         connection.query('SELECT balance FROM customers WHERE customer_id = ?', [customerId], (error, result) => {
+//             if (error) {
+//                 reject(error);
+//             } else {
+//                 resolve(result[0].balance);
+//             }
+//         });
+//     });
+// };
 
-// Function to update customer's balance in the database
-const updateCustomerBalance = async (connection, customerId, newBalance) => {
-    return new Promise((resolve, reject) => {
-        connection.query('UPDATE customers SET balance = ? WHERE customer_id = ?', [newBalance, customerId], (error, result) => {
-            if (error) {
-                reject(error);
-            } else {
-                resolve();
-            }
-        });
-    });
-};
+// // Function to update customer's balance in the database
+// const updateCustomerBalance = async (connection, customerId, newBalance) => {
+//     return new Promise((resolve, reject) => {
+//         connection.query('UPDATE customers SET balance = ? WHERE customer_id = ?', [newBalance, customerId], (error, result) => {
+//             if (error) {
+//                 reject(error);
+//             } else {
+//                 resolve();
+//             }
+//         });
+//     });
+// };
 module.exports = router;
