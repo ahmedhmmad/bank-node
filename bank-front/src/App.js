aff3d51f7af78713f7bc604802d8c8aa6444c1b7
ahmedhmmad <Ahmed.hammad@students.iugaza.edu.ps>
2024-05-08 10:17:51 +0300

    register

--- bank-front/src/App.js
+++ bank-front/src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import Login from './components/Login';
+import Register from './components/Register';
 
 const App = () => {
   return (
-    <div>
-      <h1 >NODE BANK</h1>
-      <Login />
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Login</Link>
+            </li>
+            <li>
+              <Link to="/register">Register</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
