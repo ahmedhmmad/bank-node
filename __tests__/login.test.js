738c7f1ba40bc50ce31e0f5a9602fd90d551270b
ahmedhmmad <Ahmed.hammad@students.iugaza.edu.ps>
2024-06-03 10:33:35 +0300

    trials (^_^)

--- __tests__/login.test.js
+++ __tests__/login.test.js
@@ -1,23 +1,46 @@
-const { login } = require('../controllers/authController');
-const db = require('../utils/db');
+const { login} = require('../controllers/authController'); 
+const {getUserByusername}=require('../controllers/authController');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { jwtSecret } = require('../config');
 
-jest.mock('../utils/db'); // Mocking the db module
+jest.mock('../utils/db');
+jest.mock('bcrypt');
+jest.mock('jsonwebtoken');
+jest.mock('../controllers/authController', () => ({
+    ...jest.requireActual('../controllers/authController'), 
+    getUserByusername: jest.fn(),
+}));
 
-describe('login function', () => {
-    it('should authenticate user and return access token', async () => {
-        const req = { body: { username: 'testUser', password: 'validPassword' } };
-        const res = { 
-            status: jest.fn().mockReturnThis(), 
-            json: jest.fn() 
+describe('login', () => {
+    let req;
+    let res;
+
+    beforeEach(() => {
+        req = {
+            body: {
+                username: 'testuser',
+                password: 'testpassword',
+            },
         };
+        res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn(),
+        };
+    });
 
-        // Mock db
-        db.execute.mockResolvedValueOnce([[{ username: 'testUser', password: 'hashedPassword' }]]);
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('should return 401 if user is not found', async () => {
+        getUserByusername.mockResolvedValue(null);
 
         await login(req, res);
 
-        // check
-        expect(res.status).toHaveBeenCalledWith(200);
-        expect(res.json).toHaveBeenCalledWith(expect.objectContaining({ accessToken: expect.any(String) }));
+        expect(res.status).toHaveBeenCalledWith(401);
+        expect(res.json).toHaveBeenCalledWith({ message: 'Invalid Username or Password' });
     });
+
+    
 });
