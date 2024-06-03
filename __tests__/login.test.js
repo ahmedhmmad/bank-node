892625e16d51c9bbb1197d6ff41696cc2d617344
ahmedhmmad <Ahmed.hammad@students.iugaza.edu.ps>
2024-06-03 10:45:08 +0300

    trials (^_^)

--- __tests__/login.test.js
+++ __tests__/login.test.js
@@ -3,6 +3,7 @@ const {getUserByusername}=require('../controllers/authController');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const { jwtSecret } = require('../config');
+const { get } = require('../app');
 
 jest.mock('../utils/db');
 jest.mock('bcrypt');
@@ -41,6 +42,21 @@ describe('login', () => {
         expect(res.status).toHaveBeenCalledWith(401);
         expect(res.json).toHaveBeenCalledWith({ message: 'Invalid Username or Password' });
     });
+
+    it ('should return 200 and token if login success', async ()=>
+    {
+        const user={username: 'ahmed',password: '1234'};
+        getUserByusername.mockResolvedValue(user);
+        bcrypt.compare.mockResolvedValue(true);
+        const accessToken = 'fake-jwt-token';
+        jwt.sign.mockReturnValue(accessToken);
+        await login(req,res);
+
+        expect(res.status).toHaveBeenCalledWith(200);
+        expect(res.json).toHaveBeenCalledWith({ accessToken, redirect: '/dashboard/admin', userId: user.userId });
+
+
+    })
 
     
 });
