c923cefef436a06f5b22a4804e9a44ae89c27cf2
ahmedhmmad <Ahmed.hammad@students.iugaza.edu.ps>
2024-06-10 09:10:02 +0300

    tests

--- __tests__/login.test.js
+++ __tests__/login.test.js
@@ -1,65 +1,39 @@
-const { login} = require('../controllers/authController'); 
-const {getUserByusername}=require('../controllers/authController');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const { jwtSecret } = require('../config');
-const { get } = require('../app');
-const db = require('../utils/db');
+const { login } = require("../controllers/authController");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const db = require("../utils/db");
+const { jwtSecret } = require("../config");
 
+describe("login", () => {
+  it("should return 401 and Username Or password is Invalid when Password is wrong", async () => {
+    db.execute = jest.fn().mockResolvedValueOnce([
+      [
+        {
+          id: 1,
+          username: "testuser",
+          password: "$2b$10$3t6g4",
+          role: "customer",
+        },
+      ],
+    ]);
 
-jest.mock('../utils/db');
-db.execute.mockResolvedValue([[]]);
-jest.mock('bcrypt');
-jest.mock('jsonwebtoken');
-jest.mock('../controllers/authController', () => ({
-    ...jest.requireActual('../controllers/authController'), 
-    getUserByusername: jest.fn(),
-}));
+    //Mocking bcrypt compare
+    bcrypt.compare = jest.fn().mockResolvedValueOnce(false);
 
-describe('login', () => {
-    let req;
-    let res;
-
-    beforeEach(() => {
-        req = {
-            body: {
-                username: 'testuser',
-                password: 'testpassword',
-            },
-        };
-        res = {
-            status: jest.fn().mockReturnThis(),
-            json: jest.fn(),
-        };
+    const req = {
+      body: {
+        username: "testuser",
+        password: "wrongpassword",
+      },
+    };
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+    await login(req, res);
+    expect(res.status).toHaveBeenCalledWith(401);
+    expect(res.json).toHaveBeenCalledWith({
+      message: "Invalid Username or Password",
     });
-
-    afterEach(() => {
-        jest.clearAllMocks();
-    });
-
-    it('should return 401 if user is not found', async () => {
-        getUserByusername.mockResolvedValue(null);
-
-        await login(req, res);
-
-        expect(res.status).toHaveBeenCalledWith(401);
-        expect(res.json).toHaveBeenCalledWith({ message: 'Invalid Username or Password' });
-    });
-
-    it ('should return 200 and token if login success', async ()=>
-    {
-        const user={username: 'ahmed',password: '1234'};
-        getUserByusername.mockResolvedValue(user);
-        bcrypt.compare.mockResolvedValue(true);
-        const accessToken = 'fake-jwt-token';
-        jwt.sign.mockReturnValue(accessToken);
-        await login(req,res);
-
-        expect(res.status).toHaveBeenCalledWith(200);
-        expect(res.json).toHaveBeenCalledWith({ accessToken, redirect: '/dashboard/admin', userId: user.userId });
-
-
-    });
-
-    
+  });
 });
