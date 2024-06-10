1c83e5e0ce5c87a2a03f775bae54853e6550b920
ahmedhmmad <Ahmed.hammad@students.iugaza.edu.ps>
2024-06-10 10:22:57 +0300

    more tests

--- __tests__/login.test.js
+++ __tests__/login.test.js
@@ -36,4 +36,59 @@ describe("login", () => {
       message: "Invalid Username or Password",
     });
   });
+
+//should return 200 if matched password and user found
+  it("should return 200 and accessToken, redirect, userId when Password is correct", async () => {
+    db.execute = jest.fn().mockResolvedValueOnce([
+      [
+        {
+          id: 1,
+          username: "testuser",
+          password: "$2b$10$3t6g4",
+          role: "customer",
+        },
+      ],
+    ]);
+
+    //Mocking bcrypt compare
+    bcrypt.compare = jest.fn().mockResolvedValueOnce(true);
+
+    //Mocking jwt sign
+    jwt.sign = jest.fn().mockReturnValueOnce("token");
+
+    const req = {
+      body: {
+        username: "testuser",
+        password: "password",
+      },
+    };
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+    await login(req, res);
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({
+      accessToken: "token",
+      redirect: "/dashboard",
+      userId: 1
+    });
+  });
+
+  it('should return Database Error when database error occurs', async () => {
+    db.execute=jest.fn().mockResolvedValueOnce(new Error('Database Error'));
+    const req = {
+      body: {
+        username: "testuser",
+        password: "password",
+      },
+    };
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+    await login(req, res);
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.json).toHaveBeenCalledWith({message: 'Internal Server Error'});
+  },);
 });
